0896e4f1b6020fe176d05c79769026cb116a474f
Damien Milon <37899667+dmilon@users.noreply.github.com>
2021-11-05 10:06:31 +0100

    git init

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
+end
